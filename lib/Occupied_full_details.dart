9e03d501b5bb43b6b41adefd714253c4689eb28b
Hrithunath <hrithunath777@gmail.com>
2024-03-29 12:52:54 +0530

    completed full details of occupied screen

--- lib/Occupied_full_details.dart
+++ lib/Occupied_full_details.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:room_rent_app/model/room_model.dart';
 import 'package:room_rent_app/model/user_model.dart';
 import 'package:room_rent_app/services/room_services.dart';
+import 'package:room_rent_app/services/user_services.dart';
 import 'package:room_rent_app/widgets/refactor_text.dart';
 
 class OccupiedFullDetails extends StatefulWidget {
@@ -16,84 +17,92 @@ class OccupiedFullDetails extends StatefulWidget {
 }
 
 class _OccupiedFullDetailsState extends State<OccupiedFullDetails> {
+  late UserModel _user;
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchUserDetails();
+    getuser();
+  }
+
+  Future<void> _fetchUserDetails() async {
+    final user = await fetchUserById(widget.roomModel.userId.toString());
+    setState(() {
+      _user = user!;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Full Details'),
       ),
-      body: Center(
-        child: SizedBox(
-          width: 300,
-          height: 600, // Increased height to accommodate room details
-          child: Card(
-            elevation: 30,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(20.0),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  CircleAvatar(
-                    radius: 50,
-                    backgroundImage: FileImage(File(widget.roomModel.image)),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                width: 300,
+                height: 200,
+                decoration: BoxDecoration(
+                  shape: BoxShape.rectangle,
+                  image: DecorationImage(
+                    image: FileImage(File(widget.roomModel.image)),
+                    fit: BoxFit.cover,
                   ),
-                  const SizedBox(height: 20),
-                  // Room Details
-                  customText(
-                      'Room Number', widget.roomModel.room.toString(), null),
-                  const SizedBox(height: 5),
-                  customText('Floor', widget.roomModel.floor.toString(), null),
-                  const SizedBox(height: 5),
-                  customText('Bed Type', widget.roomModel.bed, null),
-                  const SizedBox(height: 5),
-                  customText('Number of Guests',
-                      widget.roomModel.guests.toString(), null),
-                  const SizedBox(height: 20),
-                  FutureBuilder<UserModel?>(
-                    future: fetchUserById(widget.roomModel.userId.toString()),
-                    builder: (context, snapshot) {
-                      if (snapshot.connectionState == ConnectionState.waiting) {
-                        return const CircularProgressIndicator();
-                      } else if (snapshot.hasError) {
-                        return Text('Error: ${snapshot.error}');
-                      } else if (snapshot.hasData) {
-                        final user = snapshot.data!;
-                        return Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            const Text(
-                              'User Details',
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 18,
+                ),
+              ),
+              const SizedBox(height: 20),
+              // Room Details
+              ListTile(
+                title: Text('Room Number: ${widget.roomModel.room}'),
+                subtitle: Text('Floor: ${widget.roomModel.floor}'),
+              ),
+              ListTile(
+                title: Text('Bed Type: ${widget.roomModel.bed}'),
+                subtitle: Text('Number of Guests: ${widget.roomModel.guests}'),
+              ),
+              const SizedBox(height: 20),
+              _user != null
+                  ? Center(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            width: 300,
+                            height: 200,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.rectangle,
+                              image: DecorationImage(
+                                image: FileImage(File(_user.image)),
+                                fit: BoxFit.cover,
                               ),
                             ),
-                            const SizedBox(height: 10),
-                            customText('User Name', user.name, null),
-                            const SizedBox(height: 5),
-                            customText('Phone Number', user.phoneNumber, null),
-                            const SizedBox(height: 5),
-                            customText('Occupation', user.occupation, null),
-                            const SizedBox(height: 5),
-                            customText('Check-in', user.checkin, null),
-                            const SizedBox(height: 5),
-                            customText('Check-out', user.checkout, null),
-                            const SizedBox(height: 5),
-                            customText(
-                                'Advance Amount', user.advanceAmount, null),
-                          ],
-                        );
-                      } else {
-                        return const Text('No user data available');
-                      }
-                    },
-                  ),
-                ],
-              ),
-            ),
+                          ),
+                          const SizedBox(height: 20),
+                          ListTile(
+                            title: Text('User Name: ${_user.name}'),
+                            subtitle:
+                                Text('Phone Number: ${_user.phoneNumber}'),
+                          ),
+                          ListTile(
+                            title: Text('Occupation: ${_user.occupation}'),
+                            subtitle: Text('Check-in: ${_user.checkin}'),
+                          ),
+                          ListTile(
+                            title: Text('Check-out: ${_user.checkout}'),
+                            subtitle:
+                                Text('Advance Amount: ${_user.advanceAmount}'),
+                          ),
+                        ],
+                      ),
+                    )
+                  : CircularProgressIndicator(),
+            ],
           ),
         ),
       ),
